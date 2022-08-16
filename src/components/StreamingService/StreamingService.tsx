ad9fc777439a412da964268052b7518ae180dff0
bellzar7 <belelya.nazar1999@gmail.com>
2022-08-16 13:23:28 +0300

    qw

---
+++ src/components/StreamingService/StreamingService.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StreamingService = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export {StreamingService};
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export * from './User/User';
+export * from './Subscription/Subscription';
+export * from './StreamingService/StreamingService';
+export * from './Show/Show';
